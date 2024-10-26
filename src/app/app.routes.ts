545b9fc254a857102d4fad19bacc5d638012cfb1
Ludmila <ludmila27@hotmail.es>
2024-10-26 18:37:37 -0300

    Estilo en login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginFormComponent }, // Ruta principal
+  { path: 'login', component: LoginFormComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' } // Redirección para rutas no válidas
+];
